be5e1a1f81b99f2f8f3327edd626cb5f1103df6b
minyoung <69343164+Minyoung-Kyung@users.noreply.github.com>
2022-02-16 14:35:23 +0900

    Add files via upload

---
+++ Level 1/Programmers.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    var count = 0
+    var arr = Array.init(repeating: false, count: n+1)
+    
+    for i in 2...n {
+        if arr[i] == false {
+            count += 1
+            for j in stride(from: i, to: n + 1, by: i){ // i부터 n+1까지 i 간격으로 구간을 만드는 함수 stride()
+                arr[j] = true
+            }
+        }
+    }
+    
+    return count
+}
+
+solution(10)

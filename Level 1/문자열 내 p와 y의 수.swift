86756861b3e1b2ed6be5153c84b41891a9eddbb1
minyoung <69343164+Minyoung-Kyung@users.noreply.github.com>
2022-02-16 14:42:10 +0900

    Update 문자열 내 p와 y의 수.swift

--- Level 1/문자열 내 p와 y의 수.swift
+++ Level 1/문자열 내 p와 y의 수.swift
@@ -1,6 +1,5 @@
 import Foundation
 
-func solution(_ s:String) -> Bool
-{
+func solution(_ s:String) -> Bool {
     return s.lowercased().filter{$0 == "p"}.count == s.lowercased().filter{$0 == "y"}.count
 }

a544ee692042c0b9f5ba1ab50784d1ec1d69129d
minyoung <69343164+Minyoung-Kyung@users.noreply.github.com>
2022-02-16 10:54:18 +0900

    Create 이상한 문자 만들기.swift

---
+++ 이상한 문자 만들기.swift
@@ -0,0 +1,21 @@
+func solution(_ s:String) -> String {
+    let sArr = s.components(separatedBy: " ") // s를 공백으로 구분하여 배열로 저장
+    var answer : String = ""
+    
+    for i in 0..<sArr.count {
+        if i != 0 {
+            answer += " " // 공백 추가
+        }
+        
+        for j in 0..<sArr[i].count {
+            let index = sArr[i].index(sArr[i].startIndex, offsetBy: j)
+            if j % 2 == 1 {
+                answer += String(sArr[i][index]).lowercased()
+            } else {
+                answer += String(sArr[i][index]).uppercased()
+            }
+        }
+    }
+    
+    return answer
+}

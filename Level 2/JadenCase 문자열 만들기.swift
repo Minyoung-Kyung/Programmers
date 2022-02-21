8cbc248665029d9aaeaa14649345c239960d7e58
Developer <kmy9035@naver.com>
2022-02-21 22:12:43 +0900

    Create JadenCase 문자열 만들기.swift

---
+++ Level 2/JadenCase 문자열 만들기.swift
@@ -0,0 +1,21 @@
+func solution(_ s:String) -> String {
+    var sArr = s.lowercased().components(separatedBy: " ")
+    var answer = ""
+    
+    for i in 0..<sArr.count {
+        if i != 0 {
+            answer += " " // 공백 추가
+        }
+        
+        for j in 0..<sArr[i].count {
+            let index = sArr[i].index(sArr[i].startIndex, offsetBy: j)
+            if j == 0 {
+                answer += String(sArr[i][index]).uppercased() // 공백으로 나누어진 문자에서 첫번째 문자만 대문자로 변환
+            } else {
+                answer += String(sArr[i][index]).lowercased() // 나머지 문자들은 소문자로 유지
+            }
+        }
+    }
+    
+    return answer
+}

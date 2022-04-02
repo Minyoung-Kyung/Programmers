c73fa115cc2fb86ed1d6af6458f6744078b73fec
Developer <kmy9035@naver.com>
2022-04-03 00:01:00 +0900

    Update 두 정수 사이의 합.swift

--- Level 1/두 정수 사이의 합.swift
+++ Level 1/두 정수 사이의 합.swift
@@ -3,9 +3,9 @@ func solution(_ a:Int, _ b:Int) -> Int {
     var start : Int = 0
     var end : Int = 0
     
-    if (a == b) {
+    if (a == b) { // a와 b가 같은 경우는 둘 중 아무 수나 리턴
         answer = a
-    } else {   
+    } else { // 대소관계 정하기
         start = a > b ? b : a
         end = a > b ? a : b
     }

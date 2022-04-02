7e2046eb88c4f774c2b2ef78f59f43bb25de4ac8
Developer <kmy9035@naver.com>
2022-04-02 23:58:06 +0900

    Update 가운데 글자 가져오기.swift

--- Level 1/가운데 글자 가져오기.swift
+++ Level 1/가운데 글자 가져오기.swift
@@ -1,6 +1,7 @@
 func solution(_ s:String) -> String { 
-    let indexNum = s.count/2 
-    let array = Array(s) 
+    let indexNum = s.count/2
+    let array = Array(s) // 문자열을 배열로 변환
     
+    // 홀수인 경우 가운데 1글자 반환, 짝수인 경우 가운데 2글자 반환
     return s.count%2 == 1 ? String(array[indexNum]) : String(array[indexNum-1...indexNum]) 
 }

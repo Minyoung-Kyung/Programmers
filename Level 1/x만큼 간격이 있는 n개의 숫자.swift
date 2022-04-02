c7957f3ac03f6b8819c66888af5129249b1f6c16
Developer <kmy9035@naver.com>
2022-04-02 23:54:24 +0900

    Update x만큼 간격이 있는 n개의 숫자.swift

--- Level 1/x만큼 간격이 있는 n개의 숫자.swift
+++ Level 1/x만큼 간격이 있는 n개의 숫자.swift
@@ -1,11 +1,11 @@
 func solution(_ x:Int, _ n:Int) -> [Int] {
     var answer : [Int] = []
-    var startNum : Int = x
-    var addNum : Int = x
+    var startNum : Int = x // 시작하는 숫자
+    var addNum : Int = x // 증가하는 숫자
     
     for i in 1...n {
         answer.append(startNum)
-        startNum += addNum
+        startNum += addNum // x씩 증가시키며 배열에 담기
     }
     
     return answer

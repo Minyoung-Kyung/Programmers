ca0d9f292f1476dd8a42fcee62f76148b8736674
Developer <kmy9035@naver.com>
2022-04-04 21:47:35 +0900

    Update 문자열 내림차순으로 배치하기.swift

--- Level 1/문자열 내림차순으로 배치하기.swift
+++ Level 1/문자열 내림차순으로 배치하기.swift
@@ -1,3 +1,7 @@
 func solution(_ s:String) -> String {
-    return String(s.sorted{$0 > $1}) // String(s.sorted(by:>))
+    return String(s.sorted{$0 > $1})
 }
+
+// 문자열 정렬
+// 내림차순 : s.sorted(by:>), s.sorted{$0 > $1)
+// 오름차순 : s.sorted(by:<), s.sorted{$0 < $1)

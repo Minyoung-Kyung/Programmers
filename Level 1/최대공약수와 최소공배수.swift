5566fe4ca2632916c4f1785425907073b0efcede
Developer <kmy9035@naver.com>
2022-04-04 21:30:31 +0900

    Update 최대공약수와 최소공배수.swift

--- Level 1/최대공약수와 최소공배수.swift
+++ Level 1/최대공약수와 최소공배수.swift
@@ -1,13 +1,13 @@
 func solution(_ n:Int, _ m:Int) -> [Int] {
     var answer : [Int] = []
     
-    answer.append(gcd(n, m))
-    answer.append(n * m / gcd(n, m))
+    answer.append(gcd(n, m)) // 최대공약수
+    answer.append(n * m / gcd(n, m)) // 최소공배수 : 두 수의 곱 나누기 최대공약수
     
     return answer
 }
 
-func gcd(_ a: Int, _ b: Int) -> Int {
+func gcd(_ a: Int, _ b: Int) -> Int { // 최대공약수를 구하는 함수
     let r = a % b
     
     if r != 0 {

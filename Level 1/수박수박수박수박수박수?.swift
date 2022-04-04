6050b2c90fb68243e14ecff6d492a920c15c4dfb
Developer <kmy9035@naver.com>
2022-04-04 21:49:30 +0900

    Update 수박수박수박수박수박수?.swift

--- Level 1/수박수박수박수박수박수?.swift
+++ Level 1/수박수박수박수박수박수?.swift
@@ -1,7 +1,7 @@
 func solution(_ n:Int) -> String {
     var answer : String = ""
     
-    for i in 0...n-1 {
+    for i in 0...n-1 { // i가 짝수(index 번호는 0부터 시작)이면 문자열에 "수"를 더하고, 홀수이면 "박"을 더하기
         answer += i%2 == 0 ? "수" : "박"
     }
     

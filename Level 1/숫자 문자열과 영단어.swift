619dcc423d92960e4a1dd630d15c5361a6ed9621
Developer <kmy9035@naver.com>
2022-04-13 10:27:13 +0900

    Update 숫자 문자열과 영단어.swift

--- Level 1/숫자 문자열과 영단어.swift
+++ Level 1/숫자 문자열과 영단어.swift
@@ -1,9 +1,10 @@
 import Foundation
 
 func solution(_ s:String) -> Int {
-    let arr = ["zero","one","two","three","four","five","six","seven","eight","nine"]
+    let arr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     var str = s
-    for i in 0..<arr.count {
+    
+    for i in 0..<arr.count { // str이 영단어를 포함하고 있으면 숫자로 바꾸기
         str = str.replacingOccurrences(of: arr[i], with: String(i))
     }
     return Int(str)!

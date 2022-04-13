f4c282182cd41a778c2d6352123ae44635f47716
Developer <kmy9035@naver.com>
2022-04-13 10:21:03 +0900

    Update 자릿수 더하기.swift

--- Level 1/자릿수 더하기.swift
+++ Level 1/자릿수 더하기.swift
@@ -3,9 +3,9 @@ import Foundation
 func solution(_ n:Int) -> Int
 {
     var answer : Int = 0
-    var nArr = String(n).map{Int(String($0))!}
+    var nArr = String(n).map{Int(String($0))!} // n의 각 자릿수를 배열에 담기
     
-    for i in nArr {
+    for i in nArr { // 자릿수 더하기
         answer += i
     }
     

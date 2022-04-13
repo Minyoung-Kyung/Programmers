931adc76802398d0693ae01e167cd396c336e264
Developer <kmy9035@naver.com>
2022-04-13 14:06:25 +0900

    Update 정수 제곱근 판별.swift

--- Level 1/정수 제곱근 판별.swift
+++ Level 1/정수 제곱근 판별.swift
@@ -2,11 +2,11 @@ func solution(_ n:Int64) -> Int64 {
     var answer : Int64 = 0
     
     for i in 1...n {
-        if n == i*i {
+        if n == i*i { // n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴
             answer = (i+1)*(i+1)
             
             return answer
-        } else {
+        } else { // n이 양의 정수 x의 제곱이 아니라면 -1을 리턴
             answer = -1
         }
     }

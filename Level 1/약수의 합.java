eba7542ada98e4be96d7ecc734b4f546b714956e
Developer <kmy9035@naver.com>
2022-04-13 10:36:26 +0900

    Update 약수의 합.java

--- Level 1/약수의 합.java
+++ Level 1/약수의 합.java
@@ -1,9 +1,9 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
+        int answer = 0; // 약수의 합
         
         for(int i = 1; i < n + 1; i++) {
-            if (n % i == 0) {
+            if (n % i == 0) { // 나누었을 때 나머지가 0이면 약수이므로 answer에 약수를 더하기
                 answer += i;
             }
         }

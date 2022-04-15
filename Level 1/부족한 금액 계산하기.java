865ba01cf66995367d0d7f0082aeeb3e4e41c3ea
Developer <kmy9035@naver.com>
2022-04-15 12:24:20 +0900

    Update 부족한 금액 계산하기.java

--- Level 1/부족한 금액 계산하기.java
+++ Level 1/부족한 금액 계산하기.java
@@ -1,21 +1,17 @@
 class Solution {
     public long solution(int price, int money, int count) {
-        long answer = 0;
-        long result = 0; // result price
+        // 데이터 타입 long으로 선언
+        long answer = 0; // 부족한 금액
+        long result = 0; // 최종 금액
         
-        for(int i  = 1; i < count + 1; i++) {
+        for(int i  = 1; i < count + 1; i++) { // 놀이기구를 count번 째 이용한다면 원래 이용료의 count배를 받기
             result += i * price;
         }
         
-        if (result > money) {   
-            answer = result - money;    
+        if (result > money) { // 최종 금액이 자신이 가지고 있는 금액보다 많은 경우 부족한 금액 계산하기
+            answer = result - money;
         }
 
         return answer;
     }
 }
-
-/*
-1. answer, result 의 변수형 long으로 선언하기
-2. result > money 조건문 넣어주기
-*/

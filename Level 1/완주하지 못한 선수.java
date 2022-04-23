be64dafb3aa3c81102641a4c0ac3dab0d84e3f42
Developer <kmy9035@naver.com>
2022-04-24 01:17:51 +0900

    Update 완주하지 못한 선수.java

--- Level 1/완주하지 못한 선수.java
+++ Level 1/완주하지 못한 선수.java
@@ -6,13 +6,13 @@ class Solution {
     Arrays.sort(participant);
     Arrays.sort(completion);
 
-    int i = 0;
-    for(i = 0 ;i < completion.length ;i++) {
+    for(int i = 0; i < completion.length; i++) {
       if(!participant[i].equals(completion[i])) { // participant 배열에서 completion 배열에 속하지 않는 선수 찾기
           return participant[i];
       }
     }
 
-    return participant[i];
+    // 마지막 주자가 완주하지 못한 경우
+    return participant[participant.length - 1];
   }
 }

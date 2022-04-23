52c530a43bf13472b2a176f80b9de10de8729c53
Developer <kmy9035@naver.com>
2022-04-24 01:21:12 +0900

    Update 문자열 다루기 기본.swift

--- Level 1/문자열 다루기 기본.swift
+++ Level 1/문자열 다루기 기본.swift
@@ -1,4 +1,5 @@
 // 문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수
+// s가 "a234"이면 False를 리턴하고 "1234"라면 True를 리턴
 func solution(_ s:String) -> Bool {
     return (s.count == 4 || s.count == 6) && Int(s) != nil ? true : false
 }

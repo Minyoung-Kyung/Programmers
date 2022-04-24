0b2d0c3b88fe88e9f753b1ca81fd4b5798a13807
Developer <kmy9035@naver.com>
2022-04-24 14:32:14 +0900

    Update 소수 찾기.swift

--- Level 1/소수 찾기.swift
+++ Level 1/소수 찾기.swift
@@ -1,16 +1,18 @@
-// https://programmers.co.kr/learn/courses/30/lessons/12921
-// 에라토스테네스의 체
-
+// 에라토스테네스의 체 : 소수를 판별하는 알고리즘, 소수들을 대량으로 빠르고 정확하게 구하는 방법
+// 원리
+// 1. 배열을 생성하여 초기화
+// 2. 2부터 시작해서 특정 수의 배수에 해당하는 수를 모두 삭제 (지울 때 자기자신은 지우지 않고, 이미 지워진 수는 건너뛴다.)
+// 3. 2부터 시작하여 남아있는 수를 모두 출력
 import Foundation
 
 func solution(_ n:Int) -> Int {
     var count = 0
-    var arr = Array.init(repeating: false, count: n + 1)
+    var arr = Array.init(repeating: false, count: n + 1) // 배열은 0부터 시작하기 때문에 n + 1개를 false로 초기화
 
-    for i in 2...n {
-        if arr[i] == false {
+    for i in 2...n { // 가장 작은 소수인 2부터 시작
+        if arr[i] == false { // 소수인 경우
             count += 1
-            for j in stride(from: i, to: n + 1, by: i){ // i부터 n+1까지 i 간격으로 구간을 만드는 함수 stride()
+            for j in stride(from: i, to: n + 1, by: i){ // 소수인 i의 배수를 true로 바꿔준다. (i부터 n + 1까지 i씩)
                 arr[j] = true
             }
         }

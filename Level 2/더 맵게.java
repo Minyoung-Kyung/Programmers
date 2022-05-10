527365693e87be2d67f5c101db26084e12336f54
Developer <kmy9035@naver.com>
2022-05-10 23:38:50 +0900

    Update 더 맵게.java

--- Level 2/더 맵게.java
+++ Level 2/더 맵게.java
@@ -1,28 +1,28 @@
-import java.util.PriorityQueue;
-
+// 섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
 // peek() : Heap의 root 값을 확인 (root 제거 X)
 // poll() : Heap의 root 값을 확인 (root 제거 O)
+
+import java.util.PriorityQueue;
 
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
-        
         PriorityQueue<Integer> scovHeap = new PriorityQueue<>();
         
-        for (int i = 0; i < scoville.length; i++) {
-			scovHeap.add(scoville[i]);
-		} 
+        for(int i = 0; i < scoville.length; i++) {
+	    scovHeap.add(scoville[i]);
+	}
         
         while(scovHeap.peek() < K) { // 스코빌 지수가 K 이상이 되면 섞기 종료
-            
-            if (scovHeap.size() < 2) return -1;
+            if (scovHeap.size() < 2) { // 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우에는 -1을 return
+                return -1;
+            }
             
             int a = scovHeap.poll();
             int b = scovHeap.poll();
             
             scovHeap.add(a + 2 * b);
-
-			answer++;
+	    answer++;
         }
         
         return answer;

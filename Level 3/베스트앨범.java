0c8ff9d39d25120c842706787d706171cb95df52
Developer <kmy9035@naver.com>
2022-05-14 01:09:57 +0900

    Update 베스트앨범.java

--- Level 3/베스트앨범.java
+++ Level 3/베스트앨범.java
@@ -9,12 +9,12 @@ import java.util.*;
 class Solution {
     static HashMap<String, Integer> hashmap;
     static ArrayList<Integer> list;
-        
+
     public int[] solution(String[] genres, int[] plays) {
         int[] answer = {};
         list = new ArrayList<Integer>();
         hashmap = new HashMap<String, Integer>(); // <genres, plays>
-        
+
         for (int i = 0; i < genres.length; i++) {
             if (hashmap.containsKey(genres[i])) { // 이미 hashmap에 담겨있는 장르인 경우
                 int value = hashmap.get(genres[i]);
@@ -23,63 +23,63 @@ class Solution {
                 hashmap.put(genres[i], plays[i]);
             }
         }
-        
+
         // hashmap {classic: 1450, pop: 3100}
-        
+
         while (true) { // 장르의 개수만큼 while문 반복
             // 1. 속한 노래가 많이 재생된 장르
             String genre = findGenre(genres);
-            
+
             // 2. 장르 내에서 많이 재생된 노래 찾기
             findPlay(genre, genres, plays);
             hashmap.remove(genre);
-            
+
             if (hashmap.size() == 0) break;
         }
-        
-		    answer = new int[list.size()];
-        
-		    for (int i = 0; i < list.size(); i++) {
-		        answer[i] = list.get(i);
-		    }
-        
-		    return answer;
+
+        answer = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
     }
-    
+
     // 반복적으로 map의 size()가 0이 될 때까지 genre를 하나씩 찾아주는 함수
     static String findGenre(String[] genres) {
-		    int max = 0;
-		    String genre = "";
-        
-		    for (int i = 0; i < genres.length; i++) {
-		        if (hashmap.containsKey(genres[i])) {
-		            if (max < hashmap.get(genres[i])) { // 해당 장르의 vale가 max 값보다 큰 경우
-				            genre = genres[i];
-				            max = hashmap.get(genres[i]); // max 값 변경
-				        }
-		        }
+        int max = 0;
+        String genre = "";
+
+        for (int i = 0; i < genres.length; i++) {
+            if (hashmap.containsKey(genres[i])) {
+                if (max < hashmap.get(genres[i])) { // 해당 장르의 vale가 max 값보다 큰 경우
+                    genre = genres[i];
+                    max = hashmap.get(genres[i]); // max 값 변경
+                }
+            }
         }
 
         return genre;
     }
-    
+
     // 재생 횟수가 많은 것부터 찾아내 list에 저장해주는 함수
     static void findPlay(String genre, String[] genres, int[] plays) {
         for (int j = 0; j < 2; j++) {
-		        int max = 0;
-		        int index = -1;
-            
-		        for (int i = 0; i < plays.length; i++) {
-		            if (genre.equals(genres[i])) {
-		                if (max < plays[i]) {
-				                max = plays[i];
-				                index = i; // 재생 횟수가 많은 장르의 index 번호를 저장
-				            }
-				        }
-		        }
-            
+            int max = 0;
+            int index = -1;
+
+            for (int i = 0; i < plays.length; i++) {
+                if (genre.equals(genres[i])) {
+                    if (max < plays[i]) {
+                        max = plays[i];
+                        index = i; // 재생 횟수가 많은 장르의 index 번호를 저장
+                    }
+                }
+            }
+
             if (index != -1) {
-                int value = hashmap.get(genre) - plays[index]; // 재생된 횟수의 누적합에서 가장 많이 재생된 횟수를 빼기 
+                int value = hashmap.get(genre) - plays[index]; // 재생된 횟수의 누적합에서 가장 많이 재생된 횟수를 빼기
                 hashmap.put(genre, value);
                 plays[index] = -1;
                 list.add(index);

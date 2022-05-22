78fcd954dcf8a2242f02ead98c9b8dabde56d48e
Developer <kmy9035@naver.com>
2022-05-22 23:24:58 +0900

    Update 디스크 컨트롤러.swift

--- Level 3/디스크 컨트롤러.swift
+++ Level 3/디스크 컨트롤러.swift
@@ -1,6 +1,12 @@
+// 알고리즘
+// 1. 처음에는 가장 먼저 도착한 작업을 수행한다.
+// 2. 작업을 수행하는 동안 도착한 작업들을 별도의 작업 큐에 모은다.
+// 3. 작업 큐에 도착한 작업들 중 수행시간이 가장 짧은 작업을 꺼내 수행한다.
+// 모든 작업을 처리할 때까지 2, 3번 과정을 반복한다.
+
 func solution(_ jobs : [[Int]]) -> Int {
     let count = jobs.count
-    var jobs = jobs.sorted(by: {$0[0] == $1[0] ? $0[1] < $1[1] : $0[0] < $1[0] }) // 오름차순 정렬
+    var jobs = jobs.sorted(by: {$0[0] == $1[0] ? $0[1] < $1[1] : $0[0] < $1[0]}) // 오름차순 정렬
     
     let first = jobs.removeFirst()
     var last = first[0] + first[1]
